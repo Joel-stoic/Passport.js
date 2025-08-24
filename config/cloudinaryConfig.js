45d6ea73f7fea10c8cf3a0c20be54c649a90025b
Joel-stoic <joelimmanual.off@gmail.com>
2025-08-25 00:15:35 +0530

    Added some changes

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,1 +1,15 @@
-import {v2 as cloudinary } from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
+
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
+
+export default cloudinary;
+
+
+
